17db037d7d807b79cf1de34293d3cd2992aaeb92
AkashAbrrar <mabrrar9@gmail.com>
2022-07-19 22:57:02 +0600

    this commit for some test routes

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 <body>
     <div class="container">
         <h1>Hello World From Dhaka Express</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim saepe, accusamus est quisquam earum repellendus
+        <p>I am running with nodemon adipisicing elit. Enim saepe, accusamus est quisquam earum repellendus
             voluptate nihil nobis quibusdam dolores unde? Unde, perspiciatis deleniti aut excepturi numquam perferendis
             facere ut!</p>
     </div>
@@ -48,7 +48,32 @@ app.get('/', (req, res) => {
 </html>
 
     `)
-})
+});
+
+app.get('/about', (req, res, next) => {
+    res.send(
+
+        `
+        <h1>Hello There, I am the all about second gf of this page</h1>
+        `
+    )
+});
+app.get('/help', (req, res, next) => {
+    res.send(
+
+        `
+        <h1>Hello There, I am the helpful gf of this page</h1>
+        `
+    )
+});
+app.get('/contact', (req, res, next) => {
+    res.send(
+
+        `
+        <h1>Hello There, I am the contact of this page</h1>
+        `
+    )
+});
 
 app.listen(4000, () => {
     console.log("server is listening on PORT 4000");
